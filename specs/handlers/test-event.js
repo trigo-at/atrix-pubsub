cf97c8589017054a08d0d67976efb0b94033b1e7
Christian Hubinger <christian@trigo.at>
2017-03-03 13:50:02 +0100

    fake httphandler api

--- specs/handlers/test-event.js
+++ specs/handlers/test-event.js
@@ -1,13 +1,15 @@
 'use strict';
 
-
 const bb = require('bluebird');
 
 module.exports.executed = false;
 module.exports.description = 'test handler';
-module.exports.handler = async (job) => {
+
+module.exports.handler = async (req, reply, service) => {
 	await bb.delay(300);
-	module.exports.lastJob = job;
+	module.exports.reply = reply;
+	module.exports.service = service;
+	module.exports.req = req;
 	module.exports.executed = true;
 };
 
